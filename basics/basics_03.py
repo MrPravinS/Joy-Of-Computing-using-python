f818cfe48dc0cf085964e1ba53f85e80ccbf4339
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-25 20:51:41 +0530

    Define Function

---
+++ basics/basics_03.py
@@ -0,0 +1,10 @@
+# Function in python
+#  use 4 spaces per indentation level
+# declaration 
+# def funName():
+   #code
+# funcName() ,fun call
+
+def squareOfNumber(n):
+    print( n ** 2 )
+squareOfNumber(3)

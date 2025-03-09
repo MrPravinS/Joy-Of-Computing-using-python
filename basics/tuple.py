f9218b6e4e6fabd881062b482754bc4b5488f6b2
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-03-09 20:22:50 +0530

    starting tuple

---
+++ basics/tuple.py
@@ -0,0 +1,18 @@
+# Tuples in python
+# Tuples are used to store multiple items in a single variable.
+# A tuple is a collection which is ordered and unchangeable, allow duplicate values.
+
+# thisTupel = ("Pravin","Ajay","Vijay","Pravin")
+# print(len (thisTupel))
+
+# One item tuple, remember the comma:
+
+# myTuple = ("pravin")
+# print( type (myTuple))  # data type = <class str>
+
+# myTuple = ("pravin",)   # give (,) for one item
+# print( type (myTuple))  # data type = <class tuple>
+
+# tuples = ("aj",1,True,5)
+# print(tuples[-2])
+

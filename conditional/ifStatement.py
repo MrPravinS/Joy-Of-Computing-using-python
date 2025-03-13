8d7f0b1e05ffca65749ac165f866b816c90fe215
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-03-13 17:32:50 +0530

    practice on if and else statement

---
+++ conditional/ifStatement.py
@@ -0,0 +1,44 @@
+# if = Do some only if some condition is true
+#  else do something
+
+# age = int(input("Enter your age: "))
+
+# if age >= 100:
+#     print("You are too old")
+# elif age >= 18:
+#     print("You are now sign up")
+# elif age < 0:
+#     print("You havent born yet")
+# else:
+#     print("You must be 18+ to sign up")
+
+# response = input("Would you like some food (Y/N) ")
+
+# if response == "Y":
+#     print("Have Some Food")
+# elif response == "N": 
+#     print("Thank you")
+
+# name = input("Enter Your name: ")
+
+# if name == "":
+#     print("You did not tell your name")
+# else:
+#     print(f"Hello {name}")
+
+
+#  for boolean
+
+# for_sale = False
+
+# if for_sale:
+#     print("This item for sell")
+# else:
+#     print("This item not for sell")
+
+online = True 
+
+if online:
+    print("user is online")
+else:
+    print("User is Offline")

0a87a2de71346690f813ae30602f5418a82fd357
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-03-13 12:06:53 +0530

    Madlibs game

--- basics/basics_02.py
+++ basics/basics_02.py
@@ -22,11 +22,28 @@ print(f"The area is: {area}cm")"""
 
 # Shopping Cart Programm
 
-item = input("Whta item would you like to buy: ")
+"""item = input("Whta item would you like to buy: ")
 price = float(input(f"What is the price of this {item}: "))
 
 quantity = int(input("How many would you like: "))
 total = price * quantity
 
 print(f"The Item you buy {quantity} x {item}")
-print(f"Total price is ${total}")
+print(f"Total price is ${total}")"""
+
+# Madlibs game
+# word game where you create a story 
+# by filling in blanks with random workds
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective (description): ")
+
+
+print(f"Today i went to a {adjective1} zoo.")
+print(f"In an exhibit, i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")

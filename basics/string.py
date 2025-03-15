0c88b37f02ae97f08b8a14959fd4c5b70612f9a7
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-03-15 12:50:10 +0530

    string methods

---
+++ basics/string.py
@@ -0,0 +1,80 @@
+# collection of characters 
+
+#  Convert the following string to uppercase.
+
+"""string = "hello world"
+print(string.upper())"""
+
+#  Convert the following string to lowercase.
+"""
+# myStr = "HELLO WORLD"
+# print(myStr.lower())
+"""
+
+# Capitalize only the first letter of the given string.
+
+"""text = "pravin shegamwar"
+print(text.capitalize())"""
+
+# 4️⃣ Remove spaces from the beginning and end of the string.
+
+"""text = "   Python Programming   "
+print(text.strip())"""
+
+#  5️⃣ Find the position (index) of the word "Python" in the given string.
+
+"""text = "I love python programming"
+print(text.find("python"))
+"""
+
+# 6️⃣ Replace "Python" with "Java" in the given string.
+
+""""text = "I love Python programming!"
+print(text.replace("Python","Java"))"""
+
+# 7️⃣ Check if the given string starts with "Hello" and ends with "World".
+
+"""text = "Hello, welcome to the World"
+print(text.startswith("Hello"), text.endswith("World"))
+"""
+
+# Count the number of occurrences of "is" in the given string.
+
+"""text = "This is a string. It is simple"
+
+print(text.count("is"))"""
+
+# 9️⃣ Split the given string by commas and store the values in a list.
+
+"""text = "apple,banana,grape,orange"
+newText = text.split(",")   #split the string and make a list
+print(newText) """
+
+# 🔟 Join the following list into a single string, separated by hyphens (-)
+
+"""words = ["Pyhton","is","awesome"]
+newWord = "-".join(words)   #join by - 
+print(newWord)"""
+
+# 1️⃣1️⃣ Check if the given string contains only alphabetic characters.
+
+"""text = "Pyhton1233"
+print(text.isalpha())  # check only alphabetic char
+"""
+
+# 1️⃣2️⃣ Check if the given string contains only numeric digits.
+"""
+num = "1234"
+print(num.isnumeric())"""
+
+# 1️⃣3️⃣ Swap uppercase letters to lowercase and vice versa.
+
+"""text = "Python Is Fun"
+print(text.swapcase())  #swap uppercse to lower case vice versa
+"""
+
+
+# 1️⃣4️⃣ Remove all vowels from the given string.
+
+text = "Hello, how are you"
+print(text.re)

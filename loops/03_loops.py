1910a83b99c806b3826466437ce74c850bfc079a
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-03-18 23:30:18 +0530

    26. Remove Duplicates from Sorted Array

--- loops/03_loops.py
+++ loops/03_loops.py
@@ -97,4 +97,16 @@ count = 0
 # for word in strs:
 #     # if word.find(vowels):
 #        count+=1  
-#     print(word)
+#     print(word)\
+
+
+def removeDuplicates(nums):
+        k = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k+=1
+
+        return k
+print(removeDuplicates([1,1,2]))
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
